21704ca69197c89f09059f3982b0606367468cbe
klu2300030592 <saideepikareddy71@gmail.com>
2024-10-18 13:02:54 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,6 @@
+// var
+//let
+ let a = "ZeroOne";
+ a = 10;
+ console.log(a);
+ 
---
+++ deep.js
@@ -0,0 +1,34 @@
+const add =(a,b)=>
+{
+    return a+b;
+}
+console.log(add(3,4));
+
+
+
+
+const sub =(a,b)=>
+{
+    return a-b;
+}
+console.log(sub(4,3));
+
+
+
+
+const mul =(a,b)=>
+    {
+        return a*b;
+    }
+    console.log(mul(3,4));
+
+
+
+
+    
+const div =(a,b)=>
+{
+    return a/b;
+}
+console.log(div(4,2));
+    
